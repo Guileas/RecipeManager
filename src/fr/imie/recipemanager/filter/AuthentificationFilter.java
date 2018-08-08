f2defac91bf9aac5dd3988ac7815c3ce6ea19135
Coralie Himmi <coralie.himmi@evolution-xy.fr>
2018-08-08 12:08:37 +0200

    Authentification OK, Fitre OK

--- src/fr/imie/recipemanager/filter/AuthentificationFilter.java
+++ src/fr/imie/recipemanager/filter/AuthentificationFilter.java
@@ -1,5 +1,43 @@
 package fr.imie.recipemanager.filter;
 
-public class AuthentificationFilter {
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+@WebFilter("/*")
+public class AuthentificationFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filter)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		if ((req.getSession().getAttribute("pseudo") != null) || (req.getRequestURI().equals("/RecipeManager/login"))) {
+			filter.doFilter(req, response);
+			
+		} else {
+			req.getRequestDispatcher("/login.jsp").forward(req, response);
+			System.out.println("Il y a une erreur");
+		}
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
